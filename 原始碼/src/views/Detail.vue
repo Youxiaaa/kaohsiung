59c8a357e3966c82b1d00b2eb345969393748d1f
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2021-01-28 07:35:34 +0800

    原始碼上傳

---
+++ 原始碼/src/views/Detail.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="container preFade" :class="{'fadeIn': !isFade}">
+      <div class="row mt-3">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-light">
+            <li class="breadcrumb-item"><a href="#" class="text-success font-weight" @click.prevent="returnPage"> {{ currentZone }} </a></li>
+            <li class="breadcrumb-item active" aria-current="page"> {{ data.Name }} </li>
+          </ol>
+        </nav>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-6">
+          <img :src="data.Picture1" :alt="data.Picdescribe1" width="100%">
+        </div>
+        <div class="col-md-6 d-flex flex-column">
+          <br class="phoneFix">
+          <br class="phoneFix">
+          <h2 class="h2 text-danger"> {{ data.Name }} </h2>
+          <h3 class="h6 text-secondary mt-5 lh-fix"> {{ data.Description }} </h3>
+          <small class="mt-auto"> {{ data.Add }} </small>
+          <small> {{ data.Tel }} </small>
+          <small> {{ data.Changetime }} </small>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-6">
+          <p class="lh-fix">
+            <span class="text-danger h3"> <i class="fas fa-info-circle"></i> 介 紹</span>
+            <br>
+            <br>
+            {{ data.Toldescribe }}
+          </p>
+        </div>
+        <div class="col-md-6">
+          <div class="row d-flex flex-column">
+            <div class="col">
+              <p class="lh-fix">
+                <span class="text-danger h3"> <i class="fas fa-car"></i> 交 通 方 式</span>
+                <br>
+                <br>
+                {{ data.Travellinginfo }}
+                <span v-if="!data.Travellinginfo">暫無交通路線資訊</span>
+              </p>
+            </div>
+            <div class="col mt-5">
+              <p class="lh-fix">
+                <span class="text-danger h3"> <i class="far fa-clock"></i> 開 放 時 間</span>
+                <br>
+                <br>
+                <i class="far fa-clock text-danger"></i> {{ data.Opentime }}
+                <br>
+                <br>
+                <i class="fas fa-clipboard-check text-danger" v-if="data.Ticketinfo"></i> {{ data.Ticketinfo }}
+                <br>
+                <br>
+                <i class="fas fa-car text-danger" v-if="data.Parkinginfo"></i> {{ data.Parkinginfo }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: {},
+      currentZone: '',
+      currentPage: 0,
+      isFade: true
+    }
+  },
+  methods: {
+    returnPage () {
+      const vm = this
+      if (vm.currentZone === '全部區域') {
+        vm.currentZone = ''
+      }
+      vm.$router.push('/')
+      setTimeout(() => {
+        vm.$bus.$emit('getZone', vm.currentZone, vm.currentPage)
+      }, 50)
+    }
+  },
+  created () {
+    const vm = this
+
+    vm.$bus.$on('getDetail', (item, currentZone, currentPage) => {
+      vm.data = item
+      vm.currentZone = currentZone
+      vm.currentPage = currentPage
+      vm.isFade = false
+    })
+  }
+}
+</script>
---
+++ 原始碼/src/views/Home.vue
@@ -0,0 +1,187 @@
+<template>
+  <div>
+    <!-- loading -->
+    <div class="loadingAnimation" v-if="isLoading">
+      <div class="text-center">
+        <h3 class="text-success h4">載入中 請稍候...</h3>
+        <i class="fas fa-spinner fa-spin fa-3x text-danger"></i>
+      </div>
+    </div>
+
+    <!-- content -->
+    <section>
+      <div class="content preFade" :class="{'fadeIn': !isFade}">
+        <!-- leftSide -->
+        <div class="computerleftSide d-flex align-items-center justify-content-center flex-column px-4">
+          <div class="mt-auto">
+            <h1 class="h3 text-white">高 雄 旅 遊 資 訊 網</h1>
+            <select class="form-control mt-2" v-model="currentZone">
+              <option value="" disabled selected>請選擇行政區...</option>
+              <option value="">全部區域</option>
+              <option :value="item" v-for="item in locations" :key="item.id"> {{ item }} </option>
+            </select>
+          </div>
+          <div class="mt-auto">
+            <ul class="d-flex justify-content-center text-center p-0">
+              <li class="list-unstyled mx-2 h4"><a href="https://www.facebook.com/YuTsungChih" target="_blank"><i class="fab fa-facebook text-white"></i></a></li>
+              <li class="list-unstyled mx-2 h4"><a href="https://www.instagram.com/youxiaaaaaa/" target="_blank"><i class="fab fa-instagram text-white"></i></a></li>
+              <li class="list-unstyled mx-2 h4"><a href="https://github.com/Youxiaaa" target="_blank"><i class="fab fa-github-alt text-white"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="leftSide d-flex align-items-center justify-content-center flex-column px-4" :class="{'leftSideMove': sideMove}">
+          <div class="leftSideBtn">
+            <div class="d-flex justify-content-center mt-2">
+              <h2 class="text-white" @click="sideMove = !sideMove">&raquo;</h2>
+            </div>
+          </div>
+          <div class="mt-auto">
+            <h1 class="h3 text-white">高 雄 旅 遊 資 訊 網</h1>
+            <select class="form-control mt-2" v-model="currentZone">
+              <option value="" disabled selected>請選擇行政區...</option>
+              <option value="">全部區域</option>
+              <option :value="item" v-for="item in locations" :key="item.id"> {{ item }} </option>
+            </select>
+          </div>
+          <div class="mt-auto">
+            <ul class="d-flex justify-content-center text-center p-0">
+              <li class="list-unstyled mx-2 h4"><a href="https://www.facebook.com/YuTsungChih" target="_blank"><i class="fab fa-facebook text-white"></i></a></li>
+              <li class="list-unstyled mx-2 h4"><a href="https://www.instagram.com/youxiaaaaaa/" target="_blank"><i class="fab fa-instagram text-white"></i></a></li>
+              <li class="list-unstyled mx-2 h4"><a href="https://github.com/Youxiaaa" target="_blank"><i class="fab fa-github-alt text-white"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- rightSide -->
+        <div class="computerrightSide">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3 my-3" v-for="item in filterData[currentPage]" :key="item.id">
+                <div class="card h-100" @click="checkDetail(item)">
+                  <div class="card-header p-0">
+                    <img :src="item.Picture1" :alt="item.Picdescribe1" height="150px" width="100%">
+                  </div>
+                  <div class="card-body p-0 pl-3 py-3 bg-white">
+                    <h2 class="h6 text-secondary"> {{ item.Name }} </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+            <!-- pagination -->
+            <nav aria-label="Page navigation example" class="d-flex justify-content-center my-5" v-if="filterData.length > 2">
+              <ul class="pagination">
+                <li class="page-item" :class="{'pageDisabled': currentPage === 0}">
+                  <a class="page-link text-success" href="#" aria-label="Previous" @click.prevent="currentPage -= 1">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li class="page-item" v-for="item in filterData.length" :key="item.id" :class="{'active': currentPage === item - 1}"><a class="page-link text-success" href="#" @click.prevent="currentPage = item - 1" :class="{'pageActive': currentPage === item - 1}"> {{ item }} </a></li>
+                <li class="page-item" :class="{'pageDisabled': currentPage === filterData.length - 1}">
+                  <a class="page-link text-success" href="#" aria-label="Next" @click.prevent="currentPage += 1">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+        </div>
+        <div class="rightSide px-4" :class="{'rightSideMove': sideMove}">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3 my-3" v-for="item in filterData[currentPage]" :key="item.id">
+                <div class="card h-100" @click="checkDetail(item)">
+                  <div class="card-header p-0">
+                    <img :src="item.Picture1" :alt="item.Picdescribe1" height="200px" width="100%">
+                  </div>
+                  <div class="card-body p-0 pl-3 py-3 bg-white">
+                    <h2 class="h6 text-secondary"> {{ item.Name }} </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+            <!-- pagination -->
+            <div class="d-flex justify-content-center my-5" v-if="filterData.length > 1">
+              <span class="h5 text-secondary d-flex align-self-center">第</span>
+              <select v-model="currentPage" class="mx-3 p-2">
+                <option :value="item - 1" v-for="item in filterData.length" :key="item.id"> {{ item }} </option>
+              </select>
+              <span class="h5 text-secondary d-flex align-self-center">頁</span>
+            </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isLoading: false,
+      data: [],
+      currentPage: 0,
+      locations: [
+        '鹽埕區', '鼓山區', '左營區', '楠梓區', '三民區', '新興區', '前金區', '苓雅區', '前鎮區', '旗津區', '小港區', '鳳山區', '林園區', '大寮區', '大樹區', '大社區', '仁武區', '鳥松區', '岡山區', '橋頭區', '燕巢區', '田寮區', '阿蓮區', '路竹區', '湖內區', '茄萣區', '永安區', '彌陀區', '梓官區', '旗山區', '美濃區', '六龜區', '甲仙區', '杉林區', '內門區', '茂林區', '桃源區', '那瑪夏區'
+      ],
+      currentZone: '',
+      isFade: false,
+      sideMove: false
+    }
+  },
+  computed: {
+    filterData () {
+      const vm = this
+      const newData = []
+      let filterData = []
+
+      if (vm.currentZone !== '') {
+        filterData = vm.data.filter((item) => item.Add.indexOf(vm.currentZone) !== -1)
+        vm.currentPage = 0
+        vm.sideMove = false
+      } else {
+        filterData = vm.data
+        vm.sideMove = false
+      }
+
+      filterData.forEach((item, i) => {
+        if (i % 32 === 0) {
+          newData.push([])
+        }
+        const page = parseInt(i / 32)
+        newData[page].push(item)
+      })
+      return newData
+    }
+  },
+  methods: {
+    checkDetail (item) {
+      const vm = this
+      if (vm.currentZone === '') {
+        vm.currentZone = '全部區域'
+      }
+      vm.isFade = !vm.isFade
+      setTimeout(() => {
+        vm.$router.push(`detail/${item.Id}`)
+      }, 500)
+      setTimeout(() => {
+        vm.$bus.$emit('getDetail', item, vm.currentZone, vm.currentPage)
+      }, 600)
+    }
+  },
+  created () {
+    const vm = this
+    const api = 'https://api.kcg.gov.tw/api/service/get/9c8e1450-e833-499c-8320-29b36b7ace5c'
+    vm.isLoading = true
+    vm.isFade = true
+    vm.$http.get(api).then((res) => {
+      vm.isLoading = false
+      vm.isFade = false
+      vm.data = res.data.data.XML_Head.Infos.Info
+    })
+    vm.$bus.$on('getZone', (currentZone, currentPage) => {
+      vm.currentZone = currentZone
+      vm.currentPage = currentPage
+    })
+  }
+}
+</script>
